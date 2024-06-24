b4ee9768dddab33095dbc1f1c8bd0f3cf1e96b83
nulli <n.jungnickel@campus.tu-berlin.de>
2024-06-24 23:39:03 +0200

    init commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include"Header.h"
+
+
+
+int main() {
+	//Hier den Seed für Zufallszahlen
+
+	//Erstellt eine Instanz der Klasse Welt
+
+
+	while (true) {
+
+		//zeigt die Welt an
+
+		//updatet die Welt
+
+
+		//wartet 2 Sekunden
+		Sleep(2000);
+		//Löscht die alte Konsolenanzeige
+		system("cls");
+	}
+}
